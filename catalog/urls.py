15a5827df3167636970ee0eeea5c916a9ee0dd98
Diana Mayorova <dianadukai@yandex.ru>
2026-09-22 19:54:07 +0300

    Добавлены формы для создания и удаления товара

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,9 +6,26 @@ from catalog.apps import CatalogConfig
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    # Вызываем CBV через .as_view()
     path("", views.ProductListView.as_view(), name="home"),
     path("contacts/", views.ContactsView.as_view(), name="contacts"),
-    path("products/<int:pk>/", views.ProductDetailView.as_view(), name="product_detail"),
-    path("products/create/", views.ProductCreateView.as_view(), name="product_create"),
+    path(
+        "products/<int:pk>/",
+        views.ProductDetailView.as_view(),
+        name="product_detail",
+    ),
+    path(
+        "products/create/",
+        views.ProductCreateView.as_view(),
+        name="product_create",
+    ),
+    path(
+        "products/<int:pk>/update/",
+        views.ProductUpdateView.as_view(),
+        name="product_update",
+    ),
+    path(
+        "products/<int:pk>/delete/",
+        views.ProductDeleteView.as_view(),
+        name="product_delete",
+    ),
 ]
